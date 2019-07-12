bc170d68df4e5671026365be56558501ee03dded
hadimir22 <hadimir22@gmail.com>
2019-07-12 22:58:37 +0530

    target=_blank  waring fixed

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,48 +1,52 @@
-import React from 'react'
-import { Grid, Image, Icon, Label, Reveal, Button, Header } from 'semantic-ui-react'
-import faker from 'faker'
+import React from "react";
+import {
+  Grid,
+  Image,
+  Icon,
+  Label,
+  Reveal,
+  Button,
+  Header
+} from "semantic-ui-react";
+import faker from "faker";
 
-const Images = (props) => {
+const Images = props => {
+  return props.images.map(image => {
+    return (
+      <Grid.Column centered mobile={16} tablet={8} computer={8}>
+        <Reveal animated="move right">
+          <Reveal.Content visible>
+            <Image src={image.urls.regular} key={image.id} size="big" rounded />
+          </Reveal.Content>
+          <Reveal.Content hidden>
+            <div style={{ textAlign: "center", paddingTop: "50px" }}>
+              <Header as="h2" inverted>
+                Download
+              </Header>
 
-    return props.images.map((image) => {
-
-        return (
-            <Grid.Column centered mobile={16} tablet={8} computer={8} >
-                <Reveal animated='move right'>
-                    <Reveal.Content visible>
-
-                        <Image src={image.urls.regular} key={image.id} size='big' rounded />
-                    </Reveal.Content>
-                    <Reveal.Content hidden>
-                        <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-                            <Header as='h2' inverted>
-                                Download
-                             </Header>
-
-                            <Button as='div' labelPosition='right'>
-                                <Button color='red'>
-                                    <a href={image.urls.regular} target='_blank' download> <Icon name='download' />
-                                        download now</a>
-                                </Button>
-                                <Label as='a' basic color='red' pointing='left'>
-                                    {faker.random.number()} downloads
-                                </Label>
-                            </Button>
-                        </div>
-
-                    </Reveal.Content>
-                </Reveal>
-
-
-            </Grid.Column >
-
-        )
-    })
-}
-
-
-
-
-
+              <Button as="div" labelPosition="right">
+                <Button color="red">
+                  <a
+                    href={image.urls.regular}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    download
+                  >
+                    {" "}
+                    <Icon name="download" />
+                    download now
+                  </a>
+                </Button>
+                <Label as="a" basic color="red" pointing="left">
+                  {faker.random.number()} downloads
+                </Label>
+              </Button>
+            </div>
+          </Reveal.Content>
+        </Reveal>
+      </Grid.Column>
+    );
+  });
+};
 
 export default Images;
